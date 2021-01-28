54385a17f7ba1b256a64ca256c3ea8eca95b5e47
Allaga <Allaga59@yandex.ru>
2021-01-28 19:17:15 +0300

    Создан файл index.html, созданы папки js и css, в них файлы: style.css и script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let money = 5;
+let income = 10;
+let deposit = 4;
+let mission = 'Цель';
+let period = 'Период';
+
+const addExpenses = 'Добавить расходы';
+
+alert('Привет ' + mission);
+// console.log('У меня есть ' + money + ' руб., я хочу сделать вклад ' + deposit + ' руб.');
+
+console.log('addExpenses', addExpenses);
