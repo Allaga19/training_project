8c243c507cebb55956abb81e0b7b3c215778e251
Allaga <Allaga59@yandex.ru>
2021-01-28 19:41:26 +0300

    Добавлены стили в css

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert('hellow!');
+let money = 5;
+let income = 10;
+let deposit = 4;
+let mission = 'Цель';
+let period = 'Период';
+
+const addExpenses = 'Добавить расходы';
+
+alert('Привет ' + mission);
+
+console.log('addExpenses', addExpenses);
