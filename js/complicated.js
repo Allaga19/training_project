e513ba7fe4abba37d9347011216fe051a282405a
Allaga <Allaga59@yandex.ru>
2021-01-31 22:29:10 +0300

    В файл complicated.js добавлен код усложнённого задания

--- js/complicated.js
+++ js/complicated.js
@@ -1,6 +1,6 @@
 "use strict";
 
- //1) Создать переменную num со значением 266219 (тип данных число)
+//1) Создать переменную num со значением 266219 (тип данных число)
 
 let num = '266219';
 
@@ -14,6 +14,9 @@ let result = (arrmNum[0]*arrmNum[1]*arrmNum[2]*arrmNum[3]*arrmNum[4]*arrmNum[5])
 console.log(result);
 
 //3) Полученный результат возвести в степень 3, используя только 1 оператор (Math.pow не подходит)
+let resultDegree = result ** 3;
 
+console.log(resultDegree);
 
-//4) Вывести на экран первые 2 цифры полученного числа
+//4) Вывести на экран первые 2 цифры полученного числа 
+console.log(String(resultDegree).slice(0,2));
